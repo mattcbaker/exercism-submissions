09820603a92595eda4aebca8ad6f04c8e70299d1
Matt Baker <baker.mattc@gmail.com>
2016-10-18 20:18:50 -0600

    (fsharp, bob) revision using piping

--- fsharp/bob/solution/solution/Bob.fs
+++ fsharp/bob/solution/solution/Bob.fs
@@ -5,9 +5,9 @@ open System.Linq
 
 let hey (phrase : string) =
     let response = match phrase with
-        | phrase when phrase = phrase.ToUpper() && phrase.Any(fun x -> Char.IsLetter(x)) = true -> "Whoa, chill out!"
-        | phrase when phrase.EndsWith("?") -> "Sure."
-        | phrase when phrase.Trim() = "" -> "Fine. Be that way!"
+        | phrase when phrase = phrase.ToUpper() && phrase |> Seq.exists Char.IsLetter = true -> "Whoa, chill out!"
+        | phrase when "?" |> phrase.EndsWith -> "Sure."
+        | phrase when phrase |> String.IsNullOrWhiteSpace -> "Fine. Be that way!"
         | _ -> "Whatever."
 
     response
