53315da7de61ab496611c9f78f69fdfa1ac50ca8
Matt Baker <baker.mattc@gmail.com>
2016-10-18 20:12:05 -0600

    (fsharp, bob) initial submission

---
+++ fsharp/bob/solution/solution/Bob.fs
@@ -0,0 +1,13 @@
+﻿module Bob
+
+open System
+open System.Linq
+
+let hey (phrase : string) =
+    let response = match phrase with
+        | phrase when phrase = phrase.ToUpper() && phrase.Any(fun x -> Char.IsLetter(x)) = true -> "Whoa, chill out!"
+        | phrase when phrase.EndsWith("?") -> "Sure."
+        | phrase when phrase.Trim() = "" -> "Fine. Be that way!"
+        | _ -> "Whatever."
+
+    response
