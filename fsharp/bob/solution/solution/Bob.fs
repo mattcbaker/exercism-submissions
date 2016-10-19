d6259cadf1707ed99497e3c4977d2e406526a07f
Matt Baker <baker.mattc@gmail.com>
2016-10-18 20:22:22 -0600

    (fsharp, bob) removed unnecessary result variable

--- fsharp/bob/solution/solution/Bob.fs
+++ fsharp/bob/solution/solution/Bob.fs
@@ -3,10 +3,8 @@
 open System
 
 let hey (phrase : string) =
-    let response = match phrase with
+    match phrase with
         | phrase when phrase = phrase.ToUpper() && phrase |> Seq.exists Char.IsLetter = true -> "Whoa, chill out!"
         | phrase when "?" |> phrase.EndsWith -> "Sure."
         | phrase when phrase |> String.IsNullOrWhiteSpace -> "Fine. Be that way!"
         | _ -> "Whatever."
-
-    response
