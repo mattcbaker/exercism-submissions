df80346e863f725b8cdca0f8a3da6474c3e7c50b
Matt Baker <baker.mattc@gmail.com>
2016-11-07 21:59:29 -0700

    (fsharp, difference of squares) initial submission

---
+++ fsharp/difference-of-squares/solution/solution/DifferenceOfSquares.fs
@@ -0,0 +1,18 @@
+﻿module DifferenceOfSquares
+
+
+let powerOfTwo value = 
+    pown value 2
+
+let squareOfSums value = 
+    Seq.sum [1..value]
+    |> powerOfTwo
+
+let sumOfSquares value = 
+    [1..value]
+    |> Seq.map powerOfTwo
+    |> Seq.sum
+
+let difference value = 
+    squareOfSums value - sumOfSquares value
+
