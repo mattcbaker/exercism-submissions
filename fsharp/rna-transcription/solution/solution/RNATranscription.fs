0a3569bcece85c7d4c6877db8bf4be981efc7cac
Matt Baker <baker.mattc@gmail.com>
2016-11-09 18:27:50 -0700

    (fsharp, rna transcription) initial submission

---
+++ fsharp/rna-transcription/solution/solution/RNATranscription.fs
@@ -0,0 +1,10 @@
+﻿module RNATranscription
+let toRna strand = 
+    let map strand = 
+        match strand with
+        | 'G' -> 'C'
+        | 'T' -> 'A'
+        | 'A' -> 'U'        
+        | _ -> 'G'
+    
+    String.map map strand
