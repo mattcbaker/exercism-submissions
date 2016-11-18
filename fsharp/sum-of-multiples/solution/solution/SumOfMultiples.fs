7b9535f9ef54bb0d83a687275338f427011c8635
Matt Baker <baker.mattc@gmail.com>
2016-11-17 18:48:10 -0700

    (fsharp, sum of multiples) initial submission

---
+++ fsharp/sum-of-multiples/solution/solution/SumOfMultiples.fs
@@ -0,0 +1,8 @@
+﻿module SumOfMultiples
+
+let sumOfMultiples list boundary = 
+    let domain = [1..boundary-1]
+    let multipleOf number set  = 
+        Seq.exists (fun item ->  number % item = 0) set
+         
+    Seq.sumBy (fun x -> if multipleOf x list then x else 0) domain
