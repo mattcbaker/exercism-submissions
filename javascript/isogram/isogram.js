fec9a0dc8f6ca67fe80df685e933f6912a457720
Matt Baker <baker.mattc@gmail.com>
2016-11-21 13:37:11 -0700

    (javascript, isogram) consolidate regex replace and use idiomatic pattern

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,9 +1,11 @@
-module.exports = function(word) {
-    var sanizited = word.replace(/-/g, '').replace(' ', '').toLowerCase();
-
-    this.isIsogram = () => {
-        return sanizited.split('').filter((item, index, set) => {
-            return set.indexOf(item) === index;
-        }).length === sanizited.length;
-    };
+var Isogram = function(word){
+    this.sanitized = word.replace(/(-|\s)/g, '').toLowerCase();
 }
+
+Isogram.prototype.isIsogram = function() {
+        return this.sanitized.split('').filter((item, index, set) => {
+            return set.indexOf(item) === index;
+        }).length === this.sanitized.length;
+}
+
+module.exports = Isogram;
