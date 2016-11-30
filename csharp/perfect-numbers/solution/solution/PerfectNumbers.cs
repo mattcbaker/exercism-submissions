89b618720f8376e3d80c2b3acd88fe60947fc668
Matt Baker <baker.mattc@gmail.com>
2016-11-30 07:09:47 -0700

    (csharp, perfect numbers) initial submission

---
+++ csharp/perfect-numbers/solution/solution/PerfectNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+public enum NumberType
+{
+    Deficient,
+    Perfect,
+    Abundant
+}
+
+public class PerfectNumbers
+{
+    public static NumberType Classify(int number)
+    {
+        var sum = Enumerable.Range(1, number - 1)
+            .Sum(x => (number%x == 0) ? x : 0);
+
+        if (sum < number) return NumberType.Deficient;
+        if (sum == number) return NumberType.Perfect;
+
+        return NumberType.Abundant;
+    }
+}
---
+++ csharp/perfect-numbers/solution/solution/PerfectNumbersTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+[TestFixture]
+public class PerfectNumbersTest
+{
+    [TestCase(3)]
+    [TestCase(7)]
+    [TestCase(13)]
+    public void Can_classify_deficient_numbers(int number)
+    {
+        Assert.That(PerfectNumbers.Classify(number), Is.EqualTo(NumberType.Deficient));
+    }
+    
+    [TestCase(6)]
+    [TestCase(28)]
+    [TestCase(496)]
+    public void Can_classify_perfect_numbers(int number)
+    {
+        Assert.That(PerfectNumbers.Classify(number), Is.EqualTo(NumberType.Perfect));
+    }
+    
+    [TestCase(12)]
+    [TestCase(18)]
+    [TestCase(20)]
+    public void Can_classify_abundant_numbers(int number)
+    {
+        Assert.That(PerfectNumbers.Classify(number), Is.EqualTo(NumberType.Abundant));
+    }
+}
+
+public enum NumberType
+{
+    Deficient,
+    Perfect,
+    Abundant
+}
+
+public class PerfectNumbers
+{
+    public static NumberType Classify(int number)
+    {
+        var sum = Enumerable.Range(1, number - 1)
+            .Sum(x => (number%x == 0) ? x : 0);
+
+        if (sum < number) return NumberType.Deficient;
+        if (sum == number) return NumberType.Perfect;
+
+        return NumberType.Abundant;
+    }
+}
