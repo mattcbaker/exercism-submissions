7dc4e2fcbc8af195ad08622619c56ce83924782a
Matt Baker <baker.mattc@gmail.com>
2016-12-13 22:15:36 -0700

    (fsharp, space age) refactor solution

--- fsharp/space-age/solution/solution/SpaceAge.fs
+++ fsharp/space-age/solution/solution/SpaceAge.fs
@@ -5,15 +5,18 @@ type Planet = | Earth | Mercury | Venus | Mars | Jupiter | Saturn | Uranus | Nep
 let secondsInYear = 31557600m
 let earthYears seconds = 
     seconds / secondsInYear
+let rotation planet = 
+    match planet with
+        | Mercury -> 0.2408467m
+        | Venus -> 0.61519726m
+        | Mars -> 1.8808158m
+        | Jupiter -> 11.862615m
+        | Saturn -> 29.447498m
+        | Uranus -> 84.016846m
+        | Neptune -> 164.79132m
+        | Earth -> 1.m
 
 let spaceAge planet seconds = 
-    match planet with
-        | Mercury -> (earthYears seconds) / 0.2408467m
-        | Venus -> (earthYears seconds) / 0.61519726m
-        | Mars -> (earthYears seconds) / 1.8808158m
-        | Jupiter -> (earthYears seconds) / 11.862615m
-        | Saturn -> (earthYears seconds) / 29.447498m
-        | Uranus -> (earthYears seconds) / 84.016846m
-        | Neptune -> (earthYears seconds) / 164.79132m
-        | _ -> earthYears seconds
+    rotation planet
+    |> fun x -> earthYears seconds / x
     |> fun x -> System.Math.Round(x, 2)
