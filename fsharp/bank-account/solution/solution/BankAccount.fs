69e5a779000c219484b8b61a9ab9214bd3de1e48
Matt Baker <baker.mattc@gmail.com>
2016-12-17 10:55:09 -0700

    (fsharp, bank account) remove record type, it is not needed

--- fsharp/bank-account/solution/solution/BankAccount.fs
+++ fsharp/bank-account/solution/solution/BankAccount.fs
@@ -1,24 +1,14 @@
 ﻿module BankAccount
 
-type Account = 
-    {
-        amount : float
-        closed : bool
-    }
+let mkBankAccount () = Some 0.0
 
-let mkBankAccount () = 
-    {amount = 0.0; closed = false}
+let openAccount account = account
 
-let openAccount account = 
-    account
+let getBalance account = account
 
-let getBalance account = 
-    match account.closed with
-    | true -> None
-    | false -> Some account.amount
+let updateBalance (amount: float) account = 
+    match account with
+    | Some balance -> Some (balance + amount)
+    | None -> None
 
-let updateBalance amount account = 
-    { account with amount = account.amount + amount }
-
-let closeAccount account = 
-    {account with closed = true}
+let closeAccount account = None
