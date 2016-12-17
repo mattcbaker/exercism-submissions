8269fb916a6c8b3a803a8be71abd1d4bde249acc
Matt Baker <baker.mattc@gmail.com>
2016-12-17 10:42:43 -0700

    (fsharp, bank account) initial submission

---
+++ fsharp/bank-account/solution/solution/BankAccount.fs
@@ -0,0 +1,24 @@
+﻿module BankAccount
+
+type Account = 
+    {
+        amount : float
+        closed : bool
+    }
+
+let mkBankAccount () = 
+    {amount = 0.0; closed = false}
+
+let openAccount account = 
+    account
+
+let getBalance account = 
+    match account.closed with
+    | true -> None
+    | false -> Some account.amount
+
+let updateBalance amount account = 
+    { account with amount = account.amount + amount }
+
+let closeAccount account = 
+    {account with closed = true}
