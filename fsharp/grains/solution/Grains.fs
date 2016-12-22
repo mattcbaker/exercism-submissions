9db043f55c5e8bce1c7d4482bbd0e15af87feed9
Matt Baker <baker.mattc@gmail.com>
2016-12-21 22:36:11 -0700

    (fsharp, grains) update submission to use powers strategy

--- fsharp/grains/solution/Grains.fs
+++ fsharp/grains/solution/Grains.fs
@@ -1,9 +1,5 @@
 ﻿module Grains
 let square number = 
-    let rec helper current square = 
-        match square with
-        | 1 -> current
-        | _ -> helper (2I * current) (square - 1)            
-    helper 1I number
+    2I ** (number-1)
 
-let total = [1..64] |> List.map square |> List.sum
+let total = [1..64] |> List.sumBy square
