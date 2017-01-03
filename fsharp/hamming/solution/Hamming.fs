3e1f8e17a08e23ba16d3b4b8ccde89d6f5b2ecc9
Matt Baker <baker.mattc@gmail.com>
2017-01-02 22:52:44 -0700

    (fsharp, hamming) use map2 function

--- fsharp/hamming/solution/Hamming.fs
+++ fsharp/hamming/solution/Hamming.fs
@@ -1,13 +1,3 @@
 ﻿module Hamming 
     let compute first second = 
-        let rec check first second differences =
-            if String.length first > 0 && String.length second > 0 then
-                match first.[0] with
-                | c when  c <> second.[0] ->
-                    check first.[1..] second.[1..] differences+1
-                | _ -> 
-                    check first.[1..] second.[1..] differences
-            else
-                differences
-
-        check first second 0
+        Seq.map2(fun a b -> if a = b then 0 else 1) first second |> Seq.sum
