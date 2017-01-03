2cd671e1a6ac4e8d071604d0cd4d00912cc2c795
Matt Baker <baker.mattc@gmail.com>
2017-01-02 22:46:53 -0700

    (fsharp, hamming) initial submission

---
+++ fsharp/hamming/solution/Hamming.fs
@@ -0,0 +1,13 @@
+﻿module Hamming 
+    let compute first second = 
+        let rec check first second differences =
+            if String.length first > 0 && String.length second > 0 then
+                match first.[0] with
+                | c when  c <> second.[0] ->
+                    check first.[1..] second.[1..] differences+1
+                | _ -> 
+                    check first.[1..] second.[1..] differences
+            else
+                differences
+
+        check first second 0
