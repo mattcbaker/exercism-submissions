7ad4e11b278bc08f9ac94e560368b0a1fb3d6545
Matt Baker <baker.mattc@gmail.com>
2017-01-10 18:16:48 -0700

    (fsharp, nucleotide-count) initial submission

---
+++ fsharp/nucleotide-count/solution/NucleotideCount.fs
@@ -0,0 +1,10 @@
+﻿module NucleoTideCount
+    let count nucleotide strand = 
+        match nucleotide with
+        | 'A' | 'T' | 'C' | 'G' ->
+            Seq.fold (fun count current -> if current = nucleotide then count+1 else count) 0 strand
+        | _ -> 
+            raise (System.Exception("invalid nucleotide"))
+
+    let nucleotideCounts strand = 
+        List.map (fun (nucleotide, amount) -> (nucleotide,(count nucleotide strand))) [ ( 'A', 0 ); ( 'T', 0 ); ( 'C', 0 ); ( 'G', 0 ) ] |> Map.ofSeq
