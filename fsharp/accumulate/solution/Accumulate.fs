a1110a17057c4dd8853335cbf7b0345b7c360e08
Matt Baker <baker.mattc@gmail.com>
2017-01-19 21:19:02 -0700

    (fsharp, accumulate) initial submission

---
+++ fsharp/accumulate/solution/Accumulate.fs
@@ -0,0 +1,11 @@
+﻿module Accumulate
+
+let accumulate projection source = 
+    let rec apply remaining result = 
+        match remaining with 
+        | [] -> result
+        | _ ->
+            let transformed = projection (List.head remaining)
+            apply (List.tail remaining)  (result @ [transformed])
+
+    apply source []
