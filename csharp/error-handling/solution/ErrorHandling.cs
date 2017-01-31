b278efaec43db40d48481b1dd4c2328b3a970c90
Matt Baker <baker.mattc@gmail.com>
2017-01-30 22:01:43 -0700

    (chsarp, error handling) initial commit

---
+++ csharp/error-handling/solution/ErrorHandling.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class ErrorHandling
+{
+    public static void HandleErrorByThrowingException() { throw new Exception();}
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(ErrorHandlingTest.DisposableResource disposableResource)
+    {
+        disposableResource.Dispose();
+        throw new Exception();
+    }
+
+    public static int? HandleErrorByReturningNullableType(string value)
+    {
+        int parsed;
+        return (int.TryParse(value, out parsed)) ? (int?)parsed : null;
+    }
+
+    public static bool HandleErrorWithOutParam(string value, out int parsed)
+    {
+        return int.TryParse(value, out parsed);
+    }
+}
