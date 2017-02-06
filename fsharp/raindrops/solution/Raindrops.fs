2b6d343e542892aa6de3b521901b50541eeb0575
Matt Baker <baker.mattc@gmail.com>
2017-02-05 21:05:05 -0700

    (fsharp, raindrops) refactor to inline matching function

--- fsharp/raindrops/solution/Raindrops.fs
+++ fsharp/raindrops/solution/Raindrops.fs
@@ -3,12 +3,9 @@
         let getSound state factor = 
             if number % (fst factor) = 0 then state + (snd factor) else state
 
-        let matcher = 
-            function 
-            | "" -> string number
-            | sounds -> sounds
-
         [(3, "Pling"); (5, "Plang"); (7, "Plong")]
         |> List.fold getSound ""
-        |> matcher
+        |> function
+            | "" -> string number
+            | sounds -> sounds            
         
