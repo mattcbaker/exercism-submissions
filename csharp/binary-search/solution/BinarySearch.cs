b431567b28e46d8b5301fb2b6159e2b544639655
Matt Baker <baker.mattc@gmail.com>
2017-02-10 22:46:27 -0700

    (csharp, binary-search): initial submission

---
+++ csharp/binary-search/solution/BinarySearch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+public class BinarySearch
+{
+    public static int Search(int[] haystack, int needle)
+    {
+        int DivideAndConquer(int[] currentHaystack, int offset)
+        {
+            if (currentHaystack.Length == 0) return -1;
+            if (currentHaystack.Length == 1 && currentHaystack[0] != needle) return -1;
+
+            var middle = (int)Math.Floor(currentHaystack.Length / 2.0);
+
+            if (currentHaystack[middle] == needle) return middle + offset;
+
+            if (currentHaystack[middle] > needle)
+            {
+                return DivideAndConquer(currentHaystack.Take(middle).ToArray(), offset);
+            }
+
+            return DivideAndConquer(currentHaystack.Skip(middle).ToArray(), offset + middle);
+        }
+
+        return DivideAndConquer(haystack, 0);
+    }
+}
