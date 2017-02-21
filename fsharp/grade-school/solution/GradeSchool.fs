7cfe92fafdc8e7442795a2aedaf705a211d07ae3
Matt Baker <baker.mattc@gmail.com>
2017-02-20 20:27:56 -0700

    (fsharp, grade-school) minor refactor

--- fsharp/grade-school/solution/GradeSchool.fs
+++ fsharp/grade-school/solution/GradeSchool.fs
@@ -5,7 +5,8 @@ let empty = []
 let add name grade school = school @ [(name, grade)]
 
 let grade number school = 
-  List.filter (fun student -> snd student = number)  school
+  school
+  |> List.filter (fun student -> snd student = number)
   |> List.map (fun x -> fst x)
   |> List.sort
 
