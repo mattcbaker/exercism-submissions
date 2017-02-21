d76ac924c3ca1a5ff31caf26c71bd02602638a77
Matt Baker <baker.mattc@gmail.com>
2017-02-20 20:11:56 -0700

    (fsharp, grade-school) initial submission

---
+++ fsharp/grade-school/solution/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace solution.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("solution")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("solution")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("406a1bcc-8535-4631-8b6c-14a3ee08d3ef")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ fsharp/grade-school/solution/GradeSchool.fs
@@ -0,0 +1,16 @@
+﻿module GradeSchool
+
+let empty = []
+
+let add name grade school = school @ [(name, grade)]
+
+let grade number school = 
+  List.filter (fun student -> snd student = number)  school
+  |> List.map (fun x -> fst x)
+  |> List.sort
+
+let roster school = 
+  List.map (fun student -> snd student) school 
+  |> List.distinct
+  |> List.map (fun x -> x, (grade x school))
+  |> List.sort
