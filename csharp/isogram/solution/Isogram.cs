744ee9840b53416ee113fe79cef9a10c84eb3712
Matt Baker <baker.mattc@gmail.com>
2017-03-25 19:57:39 -0600

    (chsarp, isogram) avoid negation for prettierness :)

--- csharp/isogram/solution/Isogram.cs
+++ csharp/isogram/solution/Isogram.cs
@@ -3,9 +3,9 @@ using System.Linq;
 public class Isogram
 {
   public static bool IsIsogram(string phrase) => 
-    !phrase
+    phrase
     .ToLower()
     .Where(char.IsLetter)
     .GroupBy(c => c)
-    .Any(g => g.Count() > 1);
+    .All(g => g.Count() == 1);
 }
