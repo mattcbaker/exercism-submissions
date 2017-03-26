447a78aececa900e0e2f40e17dc84801564bd579
Matt Baker <baker.mattc@gmail.com>
2017-03-25 20:35:50 -0600

    (fhsarp, pangram) initial commit

---
+++ fsharp/pangram/solution/Pangram.fs
@@ -0,0 +1,10 @@
+﻿module Pangram
+
+open System
+
+let isPangram sentence = 
+  sentence
+  |> Seq.map (fun c -> Char.ToLower(c))
+  |> Seq.filter (fun c -> Char.IsLetter(c))
+  |> fun filtered -> 
+    Seq.forall (fun c -> Seq.contains c filtered) ['a'..'z']
