1cb5eab9730be51cb599cc8e0b88a31828720fbe
Matt Baker <baker.mattc@gmail.com>
2017-04-19 23:46:51 -0600

    (csharp, secret-handshake) moar formatting

--- csharp/secret-handshake/SecretHandshake.cs
+++ csharp/secret-handshake/SecretHandshake.cs
@@ -6,7 +6,10 @@ public static class SecretHandshake
 {
     public static string[] Commands(int commandValue)
     {
-        var actions = new[] {"wink", "double blink", "close your eyes", "jump"};
+        var actions = new[]
+        {
+            "wink", "double blink", "close your eyes", "jump"
+        };
         var bitsThatAreSet = Enumerable.Range(0, actions.Length)
             .Where(x => (commandValue & (1 << x)) != 0);
 
