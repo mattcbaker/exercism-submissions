3961ac2334f7b0532d97386c2b472ace15c110a5
Matt Baker <baker.mattc@gmail.com>
2017-04-19 23:45:26 -0600

    (csharp, secret-handshake) formatting

--- csharp/secret-handshake/SecretHandshake.cs
+++ csharp/secret-handshake/SecretHandshake.cs
@@ -7,9 +7,12 @@ public static class SecretHandshake
     public static string[] Commands(int commandValue)
     {
         var actions = new[] {"wink", "double blink", "close your eyes", "jump"};
-        var bitsThatAreSet = Enumerable.Range(0, actions.Length).Where(x => (commandValue & (1 << x)) != 0);
+        var bitsThatAreSet = Enumerable.Range(0, actions.Length)
+            .Where(x => (commandValue & (1 << x)) != 0);
 
-        var steps = bitsThatAreSet.Select(bit => actions[bit]).ToArray();
+        var steps = bitsThatAreSet
+            .Select(bit => actions[bit])
+            .ToArray();
 
         return (commandValue & (1 << actions.Length)) != 0
             ? steps.Reverse().ToArray()
