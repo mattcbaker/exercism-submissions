72ba08e3cd7925009075bcf6da21cabfbf3f9a2a
Matt Baker <baker.mattc@gmail.com>
2017-05-15 17:54:01 -0600

    (chsarp, simple-linked-list) solved!

---
+++ csharp/simple-linked-list/SimpleLinkedList.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SimpleLinkedList<T> : IEnumerable<T>
+{
+
+    public SimpleLinkedList(T value)
+    {
+        Value = value;
+    }
+
+    public SimpleLinkedList(IEnumerable<T> values)
+    {
+        Value = values.First();
+        var node = this;
+        foreach(var value in values.Skip(1))
+        {
+            node.Next = new SimpleLinkedList<T>(value);
+            node = node.Next;
+        }
+    }
+
+    public T Value { get; private set; }
+
+    public SimpleLinkedList<T> Next { get; private set; }
+
+    public SimpleLinkedList<T> Add(T value)
+    {
+        var node = this;
+        while(node.Next != null)
+        {
+            node = node.Next;
+        }
+        node.Next = new SimpleLinkedList<T>(value);
+        return this;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var node = this;
+        while(node != null)
+        {
+            yield return node.Value;
+            node = node.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
