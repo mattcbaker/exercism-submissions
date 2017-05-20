698348482eb53f826f75d71f651d8982213e6824
Matt Baker <baker.mattc@gmail.com>
2017-05-20 14:53:53 -0600

    (csharp, acronym) using Aggregate and a closure as solution

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -1,14 +1,34 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 public static class Acronym
 {
     public static string Abbreviate(string phrase)
     {
         phrase = phrase.Replace('-', ' ');
-        if(phrase == "HyperText Markup Language") return "HTML";
 
-        var acronym = phrase.Split(' ').Select(word => word.First());
+        var acronym = phrase.Split(' ').Select(SelectAcronymParts);
 
         return string.Join("", acronym).ToUpper();
+    }
+
+    static string SelectAcronymParts(string word){
+        var parts = string.Empty;
+
+        word.Aggregate(string.Empty, (acronym, currentLetter) => {
+            if(acronym.Length == 0){
+                parts += currentLetter.ToString().ToUpper();
+            }
+            else if(acronym.EndsWith(" ")){
+                parts += currentLetter.ToString().ToUpper();
+            }
+            else if(char.IsLower(acronym.Last()) && char.IsUpper(currentLetter)){
+                parts += currentLetter.ToString().ToUpper();
+            }
+
+            return currentLetter.ToString();
+        });
+
+        return parts;
     }
 }
