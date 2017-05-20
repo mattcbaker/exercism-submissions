8b44311bbbd474bd34bcc569c8a5b787962b26a0
Matt Baker <baker.mattc@gmail.com>
2017-05-20 15:29:30 -0600

    (csharp, acronym) trying out a for loop with explicit expressions

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -1,27 +1,34 @@
-﻿using System.Linq;
-
-public static class Acronym
+﻿public static class Acronym
 {
     public static string Abbreviate(string phrase)
     {
-        string SelectAcronym(int index) {
-            if (index == 0) return phrase[index].ToString();
+        var acronym = string.Empty;
 
-            var previousCharacter = phrase[index - 1];
-
-            if (previousCharacter == ' ') return phrase[index].ToString();
-
-            if (char.IsUpper(phrase[index]) && char.IsLower(previousCharacter)) return phrase[index].ToString();
-
-            if (previousCharacter == '-') return phrase[index].ToString();
-
-            return string.Empty;
+        for (var i = 0; i < phrase.Length; i++)
+        {
+            if (FirstChar(i))
+            {
+                acronym += phrase[i];
+            }
+            else if (PreviousCharIsSpace(i, phrase))
+            {
+                acronym += phrase[i];
+            }
+            else if(PreviousCharIsLowerCase(i, phrase) && CharIsUpperCase(i, phrase)){
+                acronym += phrase[i];
+            }
+            else if(PreviousCharIsDash(i, phrase)){
+                acronym += phrase[i];
+            }
         }
 
-        return string.Join(
-            "",
-            Enumerable.Range(0, phrase.Length-1).Select(SelectAcronym)
-        ).ToUpper();
+        return acronym.ToUpper();
     }
+
+    static bool FirstChar(int index) => index == 0;
+    static bool PreviousCharIsSpace(int index, string phrase) => phrase[index - 1] == ' ';
+    static bool PreviousCharIsLowerCase(int index, string phrase) => char.IsLower(phrase[index - 1]);
+    static bool CharIsUpperCase(int index, string phrase) => char.IsUpper(phrase[index]);
+    static bool PreviousCharIsDash(int index, string phrase) => phrase[index - 1] == '-';
 }
 
