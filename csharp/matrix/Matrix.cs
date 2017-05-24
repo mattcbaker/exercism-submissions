1b1b60a6b3ad6c53af3abfc9546c22b9336f40cf
Matt Baker <baker.mattc@gmail.com>
2017-05-24 08:56:58 -0600

    (csharp, matrix) initial submission

---
+++ csharp/matrix/Matrix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+public class Matrix
+{
+    readonly string input;
+
+    public Matrix(string input)
+    {
+        this.input = input;
+    }
+
+    public int Rows => input.Split('\n').Length;
+
+    public int Cols => input.Split('\n').First().Split(' ').Length;
+
+    public int[] Row(int row)
+    {
+        return input
+            .Split('\n')[row]
+            .Split(' ')
+            .Select(c => int.Parse(c.ToString()))
+            .ToArray();
+    }
+
+    public int[] Col(int col)
+    {
+        return input
+            .Split('\n')
+            .Select(row => int.Parse(row.Split(' ')[col]))
+            .ToArray();
+    }
+}
