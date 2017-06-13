823c09b0a80e4cb9911db68eae355f251d556482
Matt Baker <baker.mattc@gmail.com>
2017-06-13 17:55:26 -0600

    (golang, accumlate) initial submission

---
+++ go/accumulate/accumulate.go
@@ -0,0 +1,13 @@
+package accumulate
+
+const testVersion = 1
+
+func Accumulate(source []string, transform func(string) string) []string {
+	transformed := make([]string, len(source))
+
+	for i := 0; i <= len(source)-1; i++ {
+		transformed[i] = transform(source[i])
+	}
+
+	return transformed
+}
