35c3b23d7215fb238190f241229198339b88e15a
Matt Baker <baker.mattc@gmail.com>
2017-06-13 18:01:36 -0600

    (golang, accumulate) fixing silly looping error

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -5,7 +5,7 @@ const testVersion = 1
 func Accumulate(source []string, transform func(string) string) []string {
 	transformed := make([]string, len(source))
 
-	for i := 0; i <= len(source)-1; i++ {
+	for i := 0; i < len(source); i++ {
 		transformed[i] = transform(source[i])
 	}
 
