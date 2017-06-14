4ef739189b4d5c7bd5169b4539f4e8fc207b01b8
Matt Baker <baker.mattc@gmail.com>
2017-06-13 18:03:55 -0600

    (golang, accumulate) playing with different looping construct

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -5,8 +5,8 @@ const testVersion = 1
 func Accumulate(source []string, transform func(string) string) []string {
 	transformed := make([]string, len(source))
 
-	for i := 0; i < len(source); i++ {
-		transformed[i] = transform(source[i])
+	for i, v := range source {
+		transformed[i] = transform(v)
 	}
 
 	return transformed
