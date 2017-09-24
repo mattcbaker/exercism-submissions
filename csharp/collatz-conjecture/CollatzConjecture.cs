72d481aed3b7619b34c9a28349062a8e1689a6ef
Matt Baker <baker.mattc@gmail.com>
2017-09-24 10:58:12 -0600

    (csharp, collatz-conjecture) initial submission

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if(number <= 0) throw new ArgumentException();
+
+        return RecursionHelper(number, 0);
+    }
+
+    static int RecursionHelper(int number, int total)
+    {
+        if (number == 1) return total;
+
+        if (number % 2 == 0)
+        {
+            return RecursionHelper(number / 2, ++total);
+        }
+
+        return RecursionHelper(number*3+1, ++total);
+    }
+}
