46236d5cb01fc099e5e9ad22e5daa55a031b520d
Matt Baker <baker.mattc@gmail.com>
2018-03-19 21:59:38 -0400

    (csharp, reverse-string) initial submission

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        var chars = input.ToCharArray();
+        var reversed = new List<char>();
+
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            reversed.Add(chars[i]);
+        }
+
+        return string.Join(string.Empty, reversed);
+    }
+}
