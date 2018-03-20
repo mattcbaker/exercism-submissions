81842821faee3a5c95d15efbdfe381a8f6c96047
Matt Baker <baker.mattc@gmail.com>
2018-03-19 22:06:10 -0400

    (csharp, reverse-string) more simplifying

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -2,12 +2,11 @@
 {
     public static string Reverse(string input)
     {
-        var chars = input.ToCharArray();
         var reversed = string.Empty;
 
         for (int i = input.Length - 1; i >= 0; i--)
         {
-            reversed += chars[i];
+            reversed += input[i];
         }
 
         return reversed;
