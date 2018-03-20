bc1f181da3e2516032f3ae44bbea00ead65d62e5
Matt Baker <baker.mattc@gmail.com>
2018-03-19 22:04:13 -0400

    (csharp, reverse-string) use string to build up reverse, instead of List<char>

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-public static class ReverseString
+﻿public static class ReverseString
 {
     public static string Reverse(string input)
     {
         var chars = input.ToCharArray();
-        var reversed = new List<char>();
+        var reversed = string.Empty;
 
         for (int i = input.Length - 1; i >= 0; i--)
         {
-            reversed.Add(chars[i]);
+            reversed += chars[i];
         }
 
-        return string.Join(string.Empty, reversed);
+        return reversed;
     }
 }
